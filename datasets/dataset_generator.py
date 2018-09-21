10bbea5ab5fc53786c2a09395b1561f4a2c0e301
samirian <abdallahsamir99@gmail.com>
2018-09-21 12:24:03 +0200

    new files added

--- datasets/dataset_generator.py
+++ datasets/dataset_generator.py
@@ -1,8 +1,18 @@
-inputFile = open("dataset.text", "r")
+import re
+
+
+inputFile = open("dataset.txt", "r")
 outputFile = open("dataset.tsv", "w")
 
-for line in inputFile:
+data = re.split('\n', inputFile.read())
+length = len(data)
+counter = length
+for line in data:
+	print("remaining : " + counter + " of : " + length)
 	print("owner : " + line)
 	outputFile.write("owner\t" + line + "\t" + input() + "\n")
 	print("other : " + line)
 	outputFile.write("other\t" + line + "\t" + input() + "\n")
+	counter--
+
+outputFile.close()
